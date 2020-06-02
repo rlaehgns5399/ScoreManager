6e359e06da9ca5c9b977e3bb85e8646db8074b06
dohun-as <dohun@angelswing.io>
2020-06-02 22:18:06 +0900

    project: use bolierplate

---
+++ frontend/src/renderer/renderer.tsx
@@ -0,0 +1,9 @@
+import { App } from '@/renderer/components/login/index';
+import '@public/style.css';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app'),
+);
